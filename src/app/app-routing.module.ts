d482f7ff68205258ea43befd3628eb840a66c7fa
Mazvys <mazvydas@digitalsportstech.com>
2022-06-06 10:49:09 +0300

    test heroku cache

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnotherComponent } from './another/another.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:TestComponent},
+  {path:'test', component:TestComponent},
+  {path:"another",component:AnotherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
