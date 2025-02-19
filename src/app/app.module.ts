314cf9e3e5f2971d9830c70792aa515e06657eb1
Mazvys <mazvydas@digitalsportstech.com>
2025-02-20 01:12:45 +0200

    new stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { AnotherComponent } from './another/another.component';
+import { NonComp } from './comp/comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    AnotherComponent
+    AnotherComponent,
+    NonComp
   ],
   imports: [
     BrowserModule,
